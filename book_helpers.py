691e6290f058117a44b610e7c7c11913ea17be5c
frasergo <frasergo@e1bf1eea-9813-4a64-b990-0a6c360a3190>
2011-07-10 16:41:08 +0000

    Rename functions and set up a single plot showing the cycles for every offset, for each interval

--- book_helpers.py
+++ book_helpers.py
@@ -2,8 +2,10 @@
 
 """A set of functions that are called from within book.genshi.html"""
 
+import math
 import os
 import pylab
+import matplotlib.pyplot
 
 OUTPUT_DIR = "out"
 
@@ -21,11 +23,33 @@ def tone_cycle_pos(i, interval, start=0):
     c = list(tone_cycle(interval, start))
     return c.index(i)
 
-def draw_tone_cycle(filename, interval, start=0):
+def draw_tone_circle(filename, interval, start=0):
     pylab.figure(1, figsize=(2,2))
     ax = pylab.axes([0.05, 0.05, 0.95, 0.95])
     cycle_7 = list(reversed([tones[i] for i in tone_cycle(7)]))
     pylab.pie([1.0/12]*12, colors=[(0.7,)*3, (0.9,)*3], labels=cycle_7)
     pylab.savefig(os.path.join(OUTPUT_DIR, filename))
+    pylab.close()
+
+def draw_tone_cycles(filename, interval):
+    fig = matplotlib.pyplot.figure(1, figsize=(2,2))
+    ax = fig.add_axes([0.05, 0.05, 0.95, 0.95], polar=True)
+    cycle_7 = list(tone_cycle(7))
+    cycle_7.reverse()
+    s = 2*math.pi/12
+    for offset in range(0, (interval if (12 % interval == 0) else 1)):
+        tone_indexes = list(tone_cycle(interval, offset))
+        cycle = [cycle_7.index(i) for i in tone_indexes]
+        cycle.reverse()
+        l = len(cycle)
+        cycle.append(cycle[0])
+        theta = [s*i for i in cycle]
+        r = [1 for i in cycle]
+        ax.plot(theta, r)
+    ax.set_rmax(1)
+    matplotlib.pyplot.grid(False)
+    matplotlib.pyplot.axis('off')
+    fig.savefig(os.path.join(OUTPUT_DIR, filename))
+    matplotlib.pyplot.close()
 
 
