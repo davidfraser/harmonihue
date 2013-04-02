8a8a6ca34c92e54268b32103c44439b84acf7e57
frasergo <frasergo@e1bf1eea-9813-4a64-b990-0a6c360a3190>
2013-04-02 04:42:28 +0000

    Use public API rather

--- book_helpers.py
+++ book_helpers.py
@@ -462,7 +462,7 @@ def draw_torus(R=10.0, r=5.0, figsize=(10,10)):
     torus = torus_figure(ax, R, r, color='grey')
     spiral = torus_tone_spiral(ax, R, r, color='yellow')
     # stop the spiral from using the first default color
-    ax._get_lines._clear_color_cycle()
+    ax._get_lines.set_color_cycle([])
     fig.points = torus_tone_points(ax, R, r, color='orange')
     set_torus_view(ax, R, r)
     return fig
