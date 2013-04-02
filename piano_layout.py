c04b72e656b7c3c95eedd0c572b0cf9b8de7a92f
frasergo <frasergo@e1bf1eea-9813-4a64-b990-0a6c360a3190>
2013-04-02 04:41:40 +0000

    Make this sized in centimetres

--- piano_layout.py
+++ piano_layout.py
@@ -10,7 +10,6 @@ height_black = height * 2/3
 total_width = 160.0
 width_black = total_width / 12
 width_white = total_width / 7
-width_white_inner = width_white - 1
 c_tones = tones[3:] + tones[:3]
 white_notes = [index for index, tone in enumerate(c_tones + [c_tones[0]]) if len(tone) == 1]
 black_notes = [index for index, tone in enumerate(c_tones) if len(tone) >  1]
@@ -18,4 +17,7 @@ hue_cycle = list(tone_cycle(7))
 colors = get_lab_spread_colors(saturation=1.0, value=0.6)
 hue_colors = [colors[hue_cycle.index((index+3)%12)] for index in range(12)]
 bg_colors = [color.LighterColor(0.6) for color in hue_colors]
+
+figure_width = total_width / 10
+figure_height = height / 10
 
