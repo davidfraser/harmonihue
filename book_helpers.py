55c12cc36076cb77fe27479f45b8a77301a72152
David Fraser <davidf@sjsoft.com>
2014-01-12 16:46:21 +0200

    More moving around, and taking away unneccesary imports

--- book_helpers.py
+++ book_helpers.py
@@ -34,35 +34,6 @@ def hue_tone_rotation_table(interval=1, hues_function=None):
         rot = (hue_i % 4)
         yield tones[i], (r, g, b), rot
 
-def lilypond_pitch_colors(hue_function=None):
-    """generates tuples of lilypond pitch definitions and colors"""
-    # hues = get_delta_spread_hues() if hues_function is None else hues_function()
-    colors = get_lab_spread_colors()
-    hue_cycle = list(tone_cycle(7))
-    count = len(tones)
-    for note, tone in enumerate("abcdefg"):
-        base_tone_index = tones.index(tone.upper())
-        for offset, accidental in [(0, ""), (-1, "es"), (+1, "is"), (-2, "eses"), (+2, "isis")]:
-            tone_index = (base_tone_index + offset + count) % count
-            hue_index = hue_cycle.index(tone_index)
-            # lilypond pitches are C-based
-            lilypond_note = (note + 7 - 2) % 7
-            if offset:
-                yield ("0 %d %d/2" % (lilypond_note, offset), colors[hue_index])
-            else:
-                yield ("0 %d %d" % (lilypond_note, offset), colors[hue_index])
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-chromaturn_ly_filename = os.path.join(BASE_DIR, "chromaturn.ly")
-_lilypond_bool = {"#f": False, "#t": True}
-
-def lilypond_has_chromaturn():
-    check_file = os.path.join(BASE_DIR, "check-chromaturn.ly")
-    response = subprocess.check_output(["lilypond", check_file], cwd=BASE_DIR, stderr=subprocess.PIPE).strip()
-    if response in _lilypond_bool:
-        return _lilypond_bool[response]
-    raise ValueError("Unexpected response trying to check for chromaturn presence in lilypond: %s" % response)
-
 
 if __name__ == "__main__":
     import sys
