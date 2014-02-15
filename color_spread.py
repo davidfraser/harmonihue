44a322fde2c63021645ff70eb6d5020dab0b49a5
David Fraser <davidf@sjsoft.com>
2014-02-15 12:38:02 +0200

    Standardize argument names

--- color_spread.py
+++ color_spread.py
@@ -7,25 +7,24 @@ from spectrum import *
 from torus import *
 
 @figure_function
-def draw_hue_circle(n=12, hues_function=None, radius=1):
+def draw_hue_circle(count=12, hues_function=None, radius=1):
     """A diagram of a circle with hue mapped on it"""
-    fig = hue_circle_figure(n)
+    fig = hue_circle_figure(count)
     hues = default_spread_colors() if hues_function is None else hues_function()
     ax = fig.axes[0]
-    gamma = numpy.arange(-numpy.pi/12, 2*numpy.pi - numpy.pi/12, 2*numpy.pi/12)
-    radii = [radius for i in range(12)]
-    width = 2*numpy.pi/12
+    gamma = numpy.arange(-numpy.pi/count, 2*numpy.pi - numpy.pi/count, 2*numpy.pi/count)
+    radii = [radius for i in range(count)]
+    width = 2*numpy.pi/count
     bars = ax.bar(gamma, radii, width=width, bottom=0.0)
     for hue, bar in zip(hues, bars):
         bar.set_facecolor(rgb_float_tuple(hue))
     return fig
 
 @figure_function
-def draw_lab_hues(R=10.0, r=5.0, figsize=(5,5), hues_function=None):
+def draw_lab_hues(count=12, R=10.0, r=5.0, figsize=(5,5), hues_function=None):
     """Draws the spread hue values in the Lab color space"""
     fig = pyplot.figure(1, figsize=figsize)
     ax = mplot3d.Axes3D(fig)
-    count = 12
     points = count * 10
     colors = get_hue_spread(points)
     lab_colors = numpy.array([color.convert_to('lab').get_value_tuple() for color in colors])
